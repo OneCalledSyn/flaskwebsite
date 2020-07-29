e6bacea5f116ec87632be91eb461bafb4090115b
Jay Shapiro <jas2dp@virginia.edu>
2020-07-29 00:27:27 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Cryptographic key for generating tokens or signatures
 # Protection against Cross-Site Request Forgery (CSRF)
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['mltppro@gmail.com']
