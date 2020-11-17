62085051c237ce559d601a9dcd3c839db6e17820
Jay Shapiro <jas2dp@virginia.edu>
2020-11-17 17:21:53 -0500

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # Create the application object as an instance of class 'Flask'
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,15 +7,22 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -42,7 +49,12 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Syntopia startup')                        
+    app.logger.info('Syntopia startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return 'es'
 
 from app import routes, models, errors
 # Importing routes at the bottom of the file instead of at the top avoids
